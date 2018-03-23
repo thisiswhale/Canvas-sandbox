3f6f3e78470bce203d0f14446cd5c2f12c323eed
William Leung <leungwk25@gmail.com>
2018-03-22 19:04:44 -0700

    Add Draw example

--- canvas.js
+++ canvas.js
@@ -1,19 +1,26 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-//set size of canvas
+//SET SIZE OF CANVAS
 canvas.width = 578;
 canvas.height = 200;
 
+//SET SIZE to WINDOW BROWSER
+//canvas.width = window.innerWidth;
+//canvas.height = window.innerHeight;
+
 //MAKE A LINE
+ctx.beginPath();
 ctx.moveTo(0, 0);
 ctx.lineTo(578, 200);
 ctx.stroke();
 
-//TO CLEAR THINGS (x,y,height,width)
+//TO CLEAR CANVAS (x,y,width,height) ========================================-
+// x,y : starting position
+// width, height: area to clear
 //ctx.clearRect(30, 15, 100, 100);
 
-// save canvas image as data url (png format by default)
+// save canvas image as data url (png format by default)======================
 var dataURL = canvas.toDataURL();
 
 // set canvasImg image src to dataURL
@@ -21,7 +28,7 @@ var dataURL = canvas.toDataURL();
 //document.getElementById('canvasImg').src = dataURL;
 
 
-// GET MOUSE COORDINATES
+// GET MOUSE COORDINATES=====================================================
 var canvasMouse = document.getElementById("mouse");
 var ctxMouse = canvasMouse.getContext("2d");
 
@@ -49,3 +56,63 @@ canvasMouse.addEventListener('mousemove', function(evt) {
 	var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
 	writeMessage(canvasMouse, message);
 }, false);
+
+//DRAW A LINE================================================================
+
+var canvasDraw = document.getElementById("draw");
+var ctxDraw = canvasDraw.getContext("2d");
+canvasDraw.width = 578;
+canvasDraw.height = 200;
+
+ctxDraw.strokeStyle = '#BADA55';
+ctxDraw.lineJoin = 'round';
+ctxDraw.lineCap = 'round';
+
+//blend mode
+//ctx.globalCompositeOperation = 'multiply';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let direction = true;
+
+function draw(e){
+  if(!isDrawing) return; //stop the fn from running when they are not moused down
+  console.log(e);
+
+// PIXEL By PIXEL===============
+  ctxDraw.beginPath();
+  //start from
+  ctxDraw.moveTo(e.offsetX, e.offsetY);
+  //go to
+  ctxDraw.lineTo(e.offsetX, e.offsetY);
+  ctxDraw.stroke();
+
+// MORE PENCIL FEEL===============
+  // ctxDraw.beginPath();
+  // //start from
+  // ctxDraw.moveTo(lastX, lastY);
+  // //go to
+  // ctxDraw.lineTo(e.offsetX, e.offsetY);
+  // ctxDraw.stroke();
+  // [lastX, lastY] = [e.offsetX, e.offsetY];
+
+
+}
+
+canvasDraw.addEventListener('mousedown', (e) =>{
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+canvasDraw.addEventListener('mousemove', draw);
+
+canvasDraw.addEventListener('mouseup', () => isDrawing = false);
+canvasDraw.addEventListener('mouseout', () => isDrawing = false);
+
+
+//GRIDVIEW ON CANVAS =========================================================
+var canvasGrid = document.getElementById("grid");
+var ctxGrid = canvas.getContext("2d");
+canvasGrid.width = 578;
+canvasGrid.height = 200;
