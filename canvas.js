38b24854f26f54e7fe20f238862917c67647e3cb
William Leung <leungwk25@gmail.com>
2018-03-26 15:39:42 -0700

    Add Grid able to add fill color

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-var GRID = [578,200]
+var GRID = [600, 200]
 var [WIDTH , HEIGHT] = GRID;
 
 var canvas = document.getElementById("myCanvas");
@@ -15,7 +15,7 @@ canvas.height = HEIGHT;
 //MAKE A LINE
 ctx.beginPath();
 ctx.moveTo(0, 0);
-ctx.lineTo(578, 200);
+ctx.lineTo(WIDTH, HEIGHT);
 ctx.stroke();
 
 //TO CLEAR CANVAS (x,y,width,height) ========================================-
@@ -35,8 +35,8 @@ ctx.stroke();
 var canvasMouse = document.getElementById("mouse");
 var ctxMouse = canvasMouse.getContext("2d");
 
-canvasMouse.width = 578;
-canvasMouse.height = 200;
+canvasMouse.width = WIDTH;
+canvasMouse.height = HEIGHT;
 
 function writeMessage(canvas, message) {
 	var context = canvas.getContext('2d');
@@ -63,8 +63,8 @@ canvasMouse.addEventListener('mousemove', function(evt) {
 
 var canvasDraw = document.getElementById("draw");
 var ctxDraw = canvasDraw.getContext("2d");
-canvasDraw.width = 578;
-canvasDraw.height = 200;
+canvasDraw.width = WIDTH;
+canvasDraw.height = HEIGHT;
 
 //Choose color
 ctxDraw.strokeStyle = 'black';
@@ -116,28 +116,116 @@ canvasDraw.addEventListener('mouseout', () => isDrawing = false);
 //GRIDVIEW ON CANVAS =========================================================
 var canvasGrid = document.getElementById("grid");
 var ctxGrid = canvasGrid.getContext("2d");
-let gridW = 300;
-let gridH = 200;
-canvasGrid.width = 300;
-canvasGrid.height = 300;
 
-ctxGrid.strokeStyle = 'grey';
-ctxGrid.lineWidth = 1;
+canvasGrid.width = WIDTH;
+canvasGrid.height = HEIGHT;
+
 
 //ITERATE OVER WIDTH AND HEIGHT OF THE CANVAS
 //Loop for the columns
-for (let i = 0; i<= gridW; i = i+30){
-	ctxGrid.moveTo(i,0);
-	ctxGrid.lineTo(i,gridW);
-	ctxGrid.stroke();
-
-	for (let j = 0; j<= 300; j = j+30){
-		ctxGrid.moveTo(0,j);
-		ctxGrid.lineTo(300,j);
-		ctxGrid.stroke();
-	}
-
+for (let x = 0; x<= WIDTH; x = x + 10){
+	ctxGrid.moveTo(x + 0.5, 0);
+	ctxGrid.lineTo(x+ 0.5,HEIGHT);
 }
 
-//let dataGrid = new Array(WIDTH).fill(Array(HEIGHT).fill(false))
-//console.log(dataGrid);
+for (let y = 0; y<= HEIGHT; y = y+10){
+	ctxGrid.moveTo(0, 0.5+ y);
+	ctxGrid.lineTo(WIDTH, 0.5+ y);
+}
+
+ctxGrid.strokeStyle = 'grey';
+ctxGrid.lineWidth = 2;
+ctxGrid.stroke();
+//GRIDVIEW ON CANVAS AND FILL================================================
+
+var kBoardWidth = 9;
+var kBoardHeight= 9;
+var kPieceWidth = 50;
+var kPieceHeight= 50;
+var kPixelWidth = 1 + (kBoardWidth * kPieceWidth);
+var kPixelHeight= 1 + (kBoardHeight * kPieceHeight);
+
+var gridDraw = document.getElementById("gridDraw");
+var ctxGridDraw = gridDraw.getContext("2d");
+gridDraw.width = kPixelWidth;
+gridDraw.height = kPixelHeight;
+
+//ITERATE OVER WIDTH AND HEIGHT OF THE CANVAS
+//Loop for the columns
+
+ctxGridDraw.beginPath();
+
+/* vertical lines */
+for (var x = 0; x <= kPixelWidth; x += kPieceWidth) {
+    ctxGridDraw.moveTo(0.5 + x, 0);
+    ctxGridDraw.lineTo(0.5 + x, kPixelHeight);
+}
+
+/* horizontal lines */
+for (var y = 0; y <= kPixelHeight; y += kPieceHeight) {
+    ctxGridDraw.moveTo(0, 0.5 + y);
+    ctxGridDraw.lineTo(kPixelWidth, 0.5 +  y);
+}
+
+ctxGridDraw.strokeStyle = 'grey';//"#eee";
+ctxGridDraw.lineWidth = 1;
+ctxGridDraw.stroke();
+function Cell(row, column) {
+    this.row = row;
+    this.column = column;
+}
+
+function getCursorPosition(e) {
+    /* returns Cell with .row and .column properties */
+    var x;
+    var y;
+    if (e.pageX != undefined && e.pageY != undefined) {
+			x = e.pageX;
+			y = e.pageY;
+    }
+    else {
+			x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+			y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+    x -= gridDraw.offsetLeft;
+    y -= gridDraw.offsetTop;
+    x = Math.min(x, kBoardWidth * kPieceWidth);
+    y = Math.min(y, kBoardHeight * kPieceHeight);
+    var cell = new Cell(Math.floor(y/kPieceHeight), Math.floor(x/kPieceWidth));
+    return cell;
+}
+
+// function drawPiece(p, selected) {
+//     var column = p.column;
+//     var row = p.row;
+//     var x = (column * kPieceWidth) + (kPieceWidth/2);
+//     var y = (row * kPieceHeight) + (kPieceHeight/2);
+//     var radius = (kPieceWidth/2) - (kPieceWidth/10);
+//     gDrawingContext.beginPath();
+//     gDrawingContext.arc(x, y, radius, 0, Math.PI*2, false);
+//     gDrawingContext.closePath();
+//     gDrawingContext.strokeStyle = "#000";
+//     gDrawingContext.stroke();
+//     if (selected) {
+// 	gDrawingContext.fillStyle = "#000";
+// 	gDrawingContext.fill();
+//     }
+// }
+
+function fillCell(e){
+	var cell = getCursorPosition(e);
+	var column = cell.column;
+	var row = cell.row;
+	var x = (column * kPieceWidth);
+	var y = (row * kPieceHeight);
+	console.log(x,y)
+	ctxGridDraw.beginPath();
+	ctxGridDraw.fillRect(x, y,kPieceWidth,kPieceHeight);
+	ctxGridDraw.closePath();
+	ctxGridDraw.strokeStyle = "#000";
+	ctxGridDraw.stroke();
+}
+gridDraw.addEventListener('click', fillCell ,false);
+//2D ARRAY
+let dataGrid = new Array(WIDTH).fill(Array(HEIGHT).fill(false))
+//console.log(dataGrid[0]);
